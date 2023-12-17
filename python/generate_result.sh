458c570bea6f7c3a672415155c0bc8ab4db0b293
Hugo Strappazzon <hugo.strapp@hotmail.fr>
2023-12-17 18:45:36 +0100

    results added

--- python/generate_result.sh
+++ python/generate_result.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-for i in {81..100}; do
-  python3 elementaryshortestpathwithsingleslot.py -i ../data/elementaryshortestpathwithslots/instance_$i.json -c ../data/resultsV02/result_V02_instance_$i.json
+for i in {1..100}; do
+  echo "-----------"
+  echo "Instance $i"
+  echo "-----------"
+  python3 elementaryshortestpathwithsingleslot.py -i ../data/elementaryshortestpathwithslots/instance_$i.json -c ../data/Results_elementaryshortestpathwithsingleslots/result_instance_$i.json
 done
--- python/show_result.sh
+++ python/show_result.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 for i in {1..100}; do
+  echo "-----------"
+  echo "Instance $i"
+  echo "-----------"
   python3 elementaryshortestpathwithsingleslot.py -i ../data/elementaryshortestpathwithslots/instance_$i.json -c c 
 done
